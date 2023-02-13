ed02bf49c3a6295c16c86e3aa7d7a78f9d499234
KomuraAK <akileslive.farias10@gmail.com>
2023-02-13 17:58:03 -0300

    código formatado

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
-from pytube import YouTube
 import pandas as pd
-import os
+from pytube import YouTube
+from pathlib import Path
 
-def baixarMusica(url, nome, grupo):
-    url = url  # URL da música no YouTube
-    nome_musica = nome  # Nome da música
-    grupo = grupo  # Categoriza a musica em grupos
-
-    # Cria um objeto YouTube a partir da URL
-    yt = YouTube(url)
-
-    # Seleciona a melhor resolução disponível
-    melhor_resolucao = yt.streams.filter(
+def download_music(url, name, group):
+    """
+    Baixa música a partir da URL do YouTube e salva com o nome e grupo especificados.
+    """
+    youtube = YouTube(url)
+    best_resolution = youtube.streams.filter(
         progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    music_path = Path("./music") / group / f"{name}.mp4"
+    music_path.parent.mkdir(parents=True, exist_ok=True)
+    best_resolution.download(filename=str(music_path))
+    print(f"A música foi baixada com sucesso como {name}.mp4 em {group}")
 
-    # Adiciona a extensão .mp4 ao novo nome
-    nome_musica = f"{nome_musica}.mp4"
+def read_excel(data):
+    """
+    Lê dados do arquivo excel e chama a função de download de música para cada linha.
+    """
+    try:
+        download_music(data.URL, data.Nome, data.Grupo)
+    except Exception as e:
+        print(f"Erro ao baixar música {data.Nome}: {e}")
 
-    # Verifica se a pasta existe // Se não existir, é criada
-    if not os.path.exists(f"./music/{grupo}"):
-        os.makedirs(f"./music/{grupo}")
-    else:
-        print(f"A pasta {grupo} já existe e não será criada.")
-
-    # Baixa a música com o novo nome
-    melhor_resolucao.download(filename=f"./music/{grupo}/{nome_musica}")
-
-    print(f"A música foi baixada com sucesso como {nome_musica}!")
-
-
-def lerExcel(data):
-    baixarMusica(data.URL, data.Nome, data.Grupo)
-
-
-# Le o dados da planilha excel
-df = pd.read_excel("./doc/musicas.xlsx")
-
-df.apply(lerExcel, axis=1)
+def main():
+    try:
+        music_data = pd.read_excel("./doc/musicas.xlsx")
+        music_data.apply(read_excel, axis=1)
+    except FileNotFoundError as e:
+        print(f"Erro ao ler arquivo excel: {e}")
+main()
